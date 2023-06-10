221e5d66d799d4f3864804df76a49c9294958410
keiko233 <keiko23333@gmail.com>
2023-06-10 09:54:52 +0800

    refactor: use typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,82 @@
 import puppeteer from 'puppeteer';
-import { delay } from './utils';
+import { delay, mergeArray, writeJson } from './utils';
 import * as config from './config.json';
+
+puppeteer.launch({
+  // @ts-ignore
+  headless: config.puppeteer.headless,
+  devtools: config.puppeteer.devtools,
+  args: config.puppeteer.args
+}).then(async browser => {
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: config.puppeteer.viewport.width,
+    height: config.puppeteer.viewport.height
+  });
+  await page.goto(`https://twitter.com/${config.username}/${config.type[0]}`);
+  await page.waitForSelector('div[data-testid="cellInnerDiv"]');
+
+  let frequency = 0;
+  let mergedArray = [];
+  
+  while (true) {
+    if (frequency === config.frequency) {
+      mergedArray = mergeArray(mergedArray);
+      console.log(mergedArray);
+      writeJson(mergedArray);
+      break;
+    } else {
+      await delay(config.load_delay);
+
+      let result = await page.evaluate(async (config, frequency) => {
+        var modifiedUrls = [];
+
+        window.scrollTo(0, frequency * config.scroll);
+
+        function modifyParamsFromUrl(url: string, format: string, quality: string) {
+          if (quality === "original") url = url.replace(/(\?|&)name=[^&]+/g, '');
+          else url = url.replace(/(\?|&)name=[^&]+/g, '$1name=' + quality);
+          url = url.replace(/(\?|&)format=jpg/g, '$1format=' + format);
+          return url;
+        }
+
+        const tweetElements = document.querySelectorAll('div[data-testid="cellInnerDiv"]');
+
+        tweetElements.forEach(function (tweetElement) {
+          const imgElements = tweetElement.querySelectorAll('article img');
+
+          imgElements.forEach(function (imgElement) {
+            const src = imgElement.getAttribute('src');
+
+            const emojiRegex = /emoji/g;
+            const profileImagesRegex = /profile_images/g;
+            const cardImagesRegex = /card_img/g;
+            const extTwVideoThumbRegex = /ext_tw_video_thumb/g;
+            const tweetVideoThumbRegex = /tweet_video_thumb/g;
+            const amplifyVideoThumbRegex = /amplify_video_thumb/g;
+
+            if (
+              !profileImagesRegex.test(src) &&
+              !emojiRegex.test(src) &&
+              !cardImagesRegex.test(src) &&
+              !extTwVideoThumbRegex.test(src) &&
+              !tweetVideoThumbRegex.test(src) &&
+              !amplifyVideoThumbRegex.test(src)
+            ) {
+              modifiedUrls.push(modifyParamsFromUrl(src, config.format, config.quality));
+            }
+          });
+        });
+
+        return modifiedUrls;
+      }, config, frequency);
+
+      console.log(frequency);
+      console.log(result);
+      mergedArray.push(result);
+      frequency++;
+    }
+  }
+
+  await browser.close();
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,26 @@
+import fs from 'fs';
+
 export function delay(time: number) {
   return new Promise(function (resolve) {
     setTimeout(resolve, time)
   });
 };
+
+export function mergeArray(array: Array<any>) {
+  return [...new Set([].concat(...array))];
+}
+
+export function writeJson(array: Array<any>) {
+  const filename = `pic_${(new Date()).toISOString().replace(/[-:.]/g, '_').replace('T', '_').split('.')[0]}.json`;
+  fs.writeFile(filename, JSON.stringify(array), (err) => {
+    if (err) throw err;
+    console.log(filename + " is saved.");
+  });
+}
+
+export function modifyParamsFromUrl(url: string, format: string, quality: string) {
+  if (quality === "original") url = url.replace(/(\?|&)name=[^&]+/g, '');
+  else url = url.replace(/(\?|&)name=[^&]+/g, '$1name=' + quality);
+  url = url.replace(/(\?|&)format=jpg/g, '$1format=' + format);
+  return url;
+}
